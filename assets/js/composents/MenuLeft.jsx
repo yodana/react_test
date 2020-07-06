8974a98750cd828c3d7f674c0d9bc73b958a7444
DANA <dana@MacBook-Air-de-DANA.local>
2020-07-06 12:22:52 +0200

    reajustement du responsive pour le menuleft

--- assets/js/composents/MenuLeft.jsx
+++ assets/js/composents/MenuLeft.jsx
@@ -4,7 +4,8 @@ const MenuLeft = () => {
   return (
     <>
       <nav
-        className="navbar navbar-expand-lg navbar-dark bg-dark flex-column position-fixed"
+        className="navbar navbar-expand-lg d-none d-md-block
+        navbar-dark bg-dark flex-column position-fixed"
         style={{ height: 100 + "%" }}
       >
         <div className=" mr-auto" id="navbarColor02">
@@ -40,8 +41,8 @@ const MenuLeft = () => {
             </li>
           </ul>
         </div>
-        <div className="mt-5" id="navbarColor02">
-          <ul className="navbar-nav flex-column mt-5 mr-auto">
+        <div className="mt-5 mb-auto" id="navbarColor02">
+          <ul className="navbar-nav flex-column mt-5 ml-auto">
             <li className="nav-item active">
               <button className="btn btn-info mt-2 btn-block">
                 <a className="nav-link" href="#">
